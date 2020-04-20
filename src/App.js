7d89dc063c03ffc425dad893cdfb99b8af2e21f8
iamdeepti <iamdeepti956@gmail.com>
2020-04-20 20:44:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Profile from "../src/pages/profile";
+import Profile from "./pages/profile";
 function App() {
 	return (
 		<div className="App">
