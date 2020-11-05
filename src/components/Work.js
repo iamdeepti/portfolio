609a19eea8f28cfd2a923cbbe8b0ebaba71704c0
deepti singh <iamdeepti956@gmail.com>
2020-11-05 12:51:55 +0530

    modify: make tab responsive

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Tab } from "semantic-ui-react";
+import { Responsive, Tab } from "semantic-ui-react";
 const panes = [
   {
-    menuItem: "ROYAL BANK OF SCOTLAND",
+    menuItem: "Royal Bank of Scotland",
     render: () => (
       <Tab.Pane>
         <span className="White">Technology Intern | May’20-Jul’20 </span>
@@ -16,7 +16,7 @@ const panes = [
     ),
   },
   {
-    menuItem: "MORAI TECHNOLOGIES",
+    menuItem: "Morai Technologies",
     render: () => (
       <Tab.Pane>
         <span className="White">Intern | Sep’18-Oct’18</span>
@@ -30,6 +30,38 @@ const panes = [
     ),
   },
 ];
+const panesSmall = [
+	{
+	  menuItem: "RBS",
+	  render: () => (
+		<Tab.Pane>
+		  <span className="White">
+			  Royal Bank of Scotland <br/>
+			  Technology Intern | May’20-Jul’20 </span>
+		  <br />
+		  <br />
+		  Automated various manual tasks using python. Worked closely with the Dev
+		  team and implemented Keyboard accessibility guidelines and NatWest’s UI
+		  guideline on the portal for requesting resource. Added enhancements to
+		  some confluence pages of the Team.
+		</Tab.Pane>
+	  ),
+	},
+	{
+	  menuItem: "Morai Tech",
+	  render: () => (
+		<Tab.Pane>
+		  <span className="White">Intern | Sep’18-Oct’18</span>
+		  <br />
+		  <br />
+		  As a POC developed an online shopping portal using PHP and integrated
+		  Paytm for payments and Implemented all the basic functionality like
+		  shopping cart, favourites, admin portal from where more products could
+		  be added.
+		</Tab.Pane>
+	  ),
+	},
+  ];
 export class Work extends Component {
   render() {
     return (
@@ -37,11 +69,18 @@ export class Work extends Component {
         <h1 className="sub-heading">
           <span className="Green numbers">02.</span>Where I've worked?
         </h1>
-
+		<Responsive minWidth = {600}>
         <Tab
           menu={{ fluid: true, vertical: true, tabular: true }}
           panes={panes}
         />
+		</Responsive>
+		<Responsive maxWidth = {600}>
+        <Tab
+          menu={{ fluid: true, vertical: false, tabular: true }}
+          panes={panesSmall}
+        />
+		</Responsive>
       </div>
     );
   }
